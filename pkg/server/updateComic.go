05de331b350fcb78ffe96aa7c8527a9ad53fa1b3
Mai Thien Quang <thienquang2804@gmail.com>
2020-12-09 19:47:14 +0700

    Change create worker from begining and run inifite to create only when update comics

--- pkg/server/updateComic.go
+++ pkg/server/updateComic.go
@@ -18,7 +18,52 @@ var (
 	wg sync.WaitGroup
 )
 
-func worker(s *store.Stores, wg *sync.WaitGroup, comicPool <-chan model.Comic) {
+// UpdateThread read comic database and update each comic to each latest chap
+func updateComicThread(s *store.Stores, workerNum, timeout int) {
+
+	// Start update routine, then sleep for a while and re-update
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+		// Get all comics in DB
+		opt := store.NewComicsListOptions("", 0, 0)
+		comics, err := s.Comic.List(ctx, opt)
+		cancel() // Call context cancel here to avoid context leak
+
+		if err != nil {
+			logging.Info("Get list of comic fails, sleep sometimes...")
+			time.Sleep(time.Duration(timeout) * time.Minute)
+			continue
+		}
+
+		if len(comics) != 0 {
+
+			logging.Info(fmt.Sprintf("Update %d comic(s) ...", len(comics)))
+
+			// Create workers
+			comicPool := make(chan model.Comic, workerNum)
+			for i := 0; i < workerNum; i++ {
+				go worker(i, s, &wg, comicPool)
+				wg.Add(1)
+			}
+
+			// Query successful, for each comic put into job channel for worker to do the update stuffs
+			for _, comic := range comics {
+				comicPool <- comic
+			}
+			close(comicPool)
+
+			wg.Wait()
+			logging.Info("All comics is updated")
+		}
+
+		time.Sleep(time.Duration(timeout) * time.Minute)
+	}
+
+	// Never reach here
+}
+
+func worker(id int, s *store.Stores, wg *sync.WaitGroup, comicPool <-chan model.Comic) {
 
 	// Get comic from updateComicThread, which run only when updateComicThread push comic into comicPool
 	for comic := range comicPool {
@@ -33,53 +78,9 @@ func worker(s *store.Stores, wg *sync.WaitGroup, comicPool <-chan model.Comic) {
 		}
 
 		cancel() // Call context cancel here to avoid context leak
-		wg.Done()
 	}
 
-	// Never reach here
-}
-
-// UpdateThread read comic database and update each comic to each latest chap
-func updateComicThread(s *store.Stores, workerNum, timeout int) {
-
-	comicPool := make(chan model.Comic, workerNum)
-
-	// Create workers
-	for i := 0; i < workerNum; i++ {
-		go worker(s, &wg, comicPool)
-	}
-
-	// Start update routine, every 30min
-	for {
-		ctx, cancel := context.WithTimeout(context.Background(), 7*time.Second)
-
-		// Get all comics in DB
-		opt := store.NewComicsListOptions("", 0, 0)
-		comics, err := s.Comic.List(ctx, opt)
-		cancel() // Call context cancel here to avoid context leak
-
-		if err != nil {
-			logging.Info("Get list of comic fails, sleep sometimes...")
-			time.Sleep(time.Duration(timeout) * time.Minute)
-			continue
-		}
-
-		if len(comics) != 0 {
-			logging.Info(fmt.Sprintf("Update %d comic(s) ...", len(comics)))
-
-			// Query successful, for each comic put into job channel for worker to do the update stuffs
-			for _, comic := range comics {
-				comicPool <- comic
-				wg.Add(1)
-			}
-			wg.Wait()
-			logging.Info("All comics is updated")
-		}
-
-		time.Sleep(time.Duration(timeout) * time.Minute)
-	}
-
-	// Never reach here
+	wg.Done()
 }
 
 // UpdateComic use when new chapter realease
